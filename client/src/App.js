e495f2807320afb7bdd83402d7de7ba44ae77c08
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-11-24 08:17:02 +0200

    add main routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Edit from "./components/Edit";
+import Details from "./components/Details";
+import Create from "./components/Create";
+import Catalog from "./components/Catalog";
 
 function App() {
     return (
         <div id="box">
-
-            <Header />
-
-            <Home />
-            
+            <Router>
+                <Header />
+                <Route path="/" exact component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/edit" component={Edit} />
+                <Route path="/details" component={Details} />
+                <Route path="/create" component={Create} />
+                <Route path="/all-games" component={Catalog} />
+            </Router>
         </div>
     );
 }
