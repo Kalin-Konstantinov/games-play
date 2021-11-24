ee354759c10f91eda06f3afa34615a8acf112915
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-11-24 11:43:15 +0200

    load games from server and render to all-games

---
+++ client/src/components/CardGame.js
@@ -0,0 +1,20 @@
+
+
+
+const CardGame = ({
+    game
+}) => {
+    return (
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={game.imageUrl} alt="some"/>
+                <h6>{game.category}</h6>
+                <h2>{game.title}</h2>
+                <a href="/details" className="details-button">Details</a>
+            </div>
+
+        </div>
+    );
+}
+
+export default CardGame;
--- client/src/components/Catalog.js
+++ client/src/components/Catalog.js
@@ -1,38 +1,25 @@
+import CardGame from "./CardGame";
 
-
+import { useState, useEffect } from 'react';
 
 const Catalog = () => {
+    let [games, setGames] = useState([])
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+                .then(res => res.json())
+                .then(result => {
+                    setGames(result)
+                })
+        }, 1000)
+    }, []);
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-
-            <h3 className="no-articles">No articles yet</h3>
+            {games.map(game => {
+                console.log(game);
+                return games.length > 0 ? <CardGame game={game} /> : <h3 className="no-articles">No articles yet</h3>;
+            })}
         </section>
     );
 }
