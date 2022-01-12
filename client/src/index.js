c63dcdd8387997218e804294a2ab4c4c215dbf7b
Kalin Konstantinov <kalink.wow@gmail.com>
2022-01-12 10:19:36 +0200

    clean code

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-let Start = () => {
-  return (
-    <Router>
-      <App />
-    </Router>
-  );
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Start />
+     <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-reportWebVitals();
